9f4e954a33cb355e12fb92703696f5b8c0127cdf
Nedu <nedumrobert@gmail.com>
2022-06-19 11:58:56 -0400

    chore: created a post model for blog application

--- I4G002304MLQ/blog/models.py
+++ I4G002304MLQ/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_date = models.DateTimeField()
+    updated_date = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return (f"{self.title} by {self.author}").__str__()
